4bcb60d13e2c977f3040e51861c1641125a45501
Alexey Kinyov <rudy@05bit.com>
2016-04-06 13:25:37 +0300

    Add basic tests for iOS

---
+++ AmplTest.iOS/AmplitudeTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+using Uniforms.Amplitude;
+
+namespace AmplTest.iOS
+{
+    [TestFixture]
+    public class AmplitudeTests
+    {
+        [Test]
+        public void Pass()
+        {
+            Assert.True(true);
+        }
+
+        [Test]
+        public void LogEvent()
+        {
+            Amplitude.Instance.LogEvent("Test");
+        }
+    }
+}
+
